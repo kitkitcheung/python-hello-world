05f64b4a84fc18badb220ef540daa4f4f8bb8765
kitkitcheung <kitkitcheung1982@hotmail.com>
2023-05-07 16:29:54 +0800

    Add input arguement for trigger time ot take default tigger time

--- selenium_taobao.py
+++ selenium_taobao.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import datetime
+import argparse
 
 
 def click_element(element_type, element_key, maximum_of_retry):
@@ -21,17 +22,35 @@ def click_element(element_type, element_key, maximum_of_retry):
         time.sleep(1)
 
     if not is_found:
-        print(f"Error: Exit after 5 tries but not found the element {element_key}.")
+        print(f"Error: Exit after {maximum_of_retry} tries but not found the element {element_key}.")
 
 
-time_to_trigger = "2023-05-07 15:35:00.00000000"
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser()
+
+
+#If not input a time to trigger, then default time is current time plus 1 minute
+dt_default_lambda = lambda: datetime.datetime.now() + datetime.timedelta(minutes=1)
+trigger_time_default = dt_default_lambda().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+# Add an argument to the parser
+parser.add_argument('--datetime', type=str, default=trigger_time_default, help='Input time for trigger, e.g.: 2023-05-07 15:35:00.00000000')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Use the date time value
+time_to_trigger = args.datetime
+print(f"Time to trigger: {time_to_trigger}")
+
+# time_to_trigger = "2023-05-07 15:35:00.00000000"
 
 # Create a new ChromeDriver instance
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging']) # disable Chrome logging output
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.taobao.com")
-time.sleep(3)
+time.sleep(1)
 
 # Show the login page to login
 driver.find_element(By.LINK_TEXT, "亲，请登录").click()
@@ -40,7 +59,7 @@ time.sleep(30)
 
 #open shopping cart page to select the specified item
 driver.get("https://cart.taobao.com/cart.htm")
-time.sleep(5)
+time.sleep(1)
 
 click_element(By.ID, 'J_SelectAll1', 5)
 
@@ -57,6 +76,6 @@ while True:
         break
     time.sleep(0.01)
 
-time.sleep(30)
+time.sleep(60)
 # Close the browser
 driver.quit()
